ba99be16dfbce342760ea0753adf1f5846da50a1
sitinajundasari <91586288+sitinajundasari@users.noreply.github.com>
2021-10-22 12:28:47 +0700

    Add files via upload

---
+++ HeroesChallenge/main.java
@@ -0,0 +1,38 @@
+class Main {
+    public static void main(String[] args) {
+      heroes hero = new heroes();
+  
+      hero.setProfile("Siti Najunda Sari", 20, 165);
+      hero.profile();
+      
+  
+      
+      hero.jalan();
+
+      hero.lari();
+      hero.lari();
+
+      hero.makan();
+
+      hero.minum();
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+        
+      
+      hero.profile();
+      
+    }
+  }
